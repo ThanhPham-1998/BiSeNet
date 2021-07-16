3243ae2b94fe24b6818effd1913298549fbf91dc
TieuThanh <72813688+TieuThanh@users.noreply.github.com>
2021-07-16 18:31:47 +0700

    Update loss.py

--- loss.py
+++ loss.py
@@ -10,7 +10,7 @@ class BiSiNetLoss(nn.Module):
     def forward(self, pred, target, features_1=0, features_2=0):
         if self.training:
             loss_1 = self.loss(pred, target)
-            loss_2 = self.loss(feature_1, target)
-            loss_3 = self.loss(feature_2, target)
+            loss_2 = self.loss(features_1, target)
+            loss_3 = self.loss(features_2, target)
             return (loss_1 + loss_2 + loss_3) / 3
         return self.loss(pred, target)
